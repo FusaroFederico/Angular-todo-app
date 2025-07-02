31e0d5071ac51b8d9edd0ca2848a02d026e93865
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2025-07-02 14:52:04 +0200

    feat: now tasks are saved in localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -9,19 +9,38 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TODO List';
   tasks: string[] = ['Comprare il pane', 'Studiare Angular', 'Fare sport'];
   newTask: string = '';
+  
+  // carica le tasks quando il componente viene avviato
+  ngOnInit(){
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks){
+      this.tasks = JSON.parse(savedTasks);
+    }
+  }
+
+  // salva le tasks in localStorage
+  saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
 
   addTask() {
     if (this.newTask.trim() !== '') {
       this.tasks.push(this.newTask.trim());
       this.newTask = '';
+      this.saveTasks();
     }
   }
 
   removeTask(index: number) {
-    this.tasks.splice(index, 1);
+    // chiede conferma prima di rimuovere la task
+    if(confirm('Sei sicuor di voler rimuovere questa attività?')){
+      this.tasks.splice(index, 1);
+      this.saveTasks();
+    }
   }
 }
